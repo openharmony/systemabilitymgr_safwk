991ffdfabaf5065cebf5cf7d7b6304f3f359878f
huangxiaolinabc123 <huangxiaolin12@huawei.com>
2023-04-01 16:37:31 +0800

    modify code
    Signed-off-by: huangxiaolinabc123 <huangxiaolin12@huawei.com>
    Change-Id: I1b57a3c2992618b850ce0d6de75f8e2c1c994b91

--- services/safwk/include/system_ability.h
+++ services/safwk/include/system_ability.h
@@ -64,34 +64,157 @@ enum class SystemAbilityState {
     ACTIVE,
     IDLE,
 };
-
+/**
+ * @class SystemAbility
+ * SystemAbility provides a base class for each SA
+ */
 class SystemAbility {
     DECLARE_BASE_SYSTEM_ABILITY(SystemAbility);
 
 public:
+    /**
+     * MakeAndRegisterAbility, Register SA into the localAbilityManager.
+     *
+     * @param systemAbility,.SA required to register
+     * @return Returns true on success.
+     */
     static bool MakeAndRegisterAbility(SystemAbility* systemAbility);
+
+    /**
+     * AddSystemAbilityListener, Provide the function of SA monitoring and waiting for other SA
+     *
+     * @param systemAbilityId,.said that needs to be monitored
+     * @return Returns true on success.
+     */
     bool AddSystemAbilityListener(int32_t systemAbilityId);
+
+    /**
+     * RemoveSystemAbilityListener, Remove the SA you are listening to
+     *
+     * @param systemAbilityId,.The listening said that needs to be removed
+     * @return Returns true on success.
+     */
     bool RemoveSystemAbilityListener(int32_t systemAbilityId);
 
 protected:
+    /**
+     * OnDump, Reserved for use, not currently used
+     *
+     * @return Returns void.
+     */
     virtual void OnDump();
+
+    /**
+     * OnStart, The user needs to rewrite onstart, initialize it in onstart, and publish SA
+     *
+     * @return Returns void.
+     */
     virtual void OnStart();
+
+    /**
+     * OnStart, The user needs to rewrite onstart, initialize it in onstart, and publish SA
+     *
+     * @param startReason,.The reason for pulling SA. There are three keys in the reason:
+    eventId, name, and value. They correspond to the type, name, and value in the configuration file
+     * @return Returns void.
+     */
     virtual void OnStart(const std::unordered_map<std::string, std::string>& startReason);
+
+    /**
+     * OnIdle, The user needs to rewrite OnIdle, OnIdle is a callback function when uninstalling SA
+     *
+     * @param idleReason,.The reason for unload SA. There are three keys in the reason:
+    eventId, name, and value. They correspond to the type, name, and value in the configuration file
+     * @return Returning 0s indicates that you agree to uninstall, returning - 1 indicates that you
+    refuse to uninstall,and returning 0-5 * 60 * 1000 indicates that SA has delayed uninstalling
+     */
     virtual int32_t OnIdle(const std::unordered_map<std::string, std::string>& idleReason);
+
+    /**
+     * OnActive, The user needs to rewrite OnActive, OnActive is a callback function when Call CancelIdle
+     *
+     * @param activeReason,.The reason for active SA. There are three keys in the reason:
+    eventId, name, and value. They correspond to the type, name, and value in the configuration file
+     * @return void
+     */
     virtual void OnActive(const std::unordered_map<std::string, std::string>& activeReason);
+
+    /**
+     * OnStop, The user needs to rewrite OnActive, Onstop is called when the process is uninstalled
+     *
+     * @return void
+     */
     virtual void OnStop();
+
+    /**
+     * OnStop, The user needs to rewrite OnActive, Onstop is called when the SA is uninstalled
+     *
+     * @param stopReason,.The reason for stop SA. There are three keys in the reason:
+     eventId, name, and value. They correspond to the type, name, and value in the configuration file
+     * @return void
+     */
     virtual void OnStop(const std::unordered_map<std::string, std::string>& stopReason);
+
+    /**
+     * OnAddSystemAbility, The user needs to rewrite OnAddSystemAbility, OnAddSystemAbility will be
+     called when the listening SA starts
+     *
+     * @param systemAbilityId,.The said being listened to
+     * @param deviceId,.deviceId is empty, Indicates support for local, temporarily not cross device
+     * @return void
+     */
     virtual void OnAddSystemAbility(int32_t systemAbilityId, const std::string& deviceId);
+
+    /**
+     * OnRemoveSystemAbility, The user needs to rewrite OnRemoveSystemAbility, OnRemoveSystemAbility will be
+     called when the listening SA Uninstall
+     *
+     * @param systemAbilityId,.The said being listened to
+     * @param deviceId,.deviceId is empty, Indicates support for local, temporarily not cross device
+     * @return void
+     */
     virtual void OnRemoveSystemAbility(int32_t systemAbilityId, const std::string& deviceId);
 
+    /**
+     * GetSystemAbility, get systemability
+     *
+     * @param systemAbilityId,.The said number that needs to be obtained
+     * @return nullptr indicates that sa does not exist
+     */
     sptr<IRemoteObject> GetSystemAbility(int32_t systemAbilityId);
+
+    /**
+     * Publish, Register functions for sa
+     *
+     * @param systemAbility,.SA that need to be registered
+     * @return True indicates successful registration
+     */
     bool Publish(sptr<IRemoteObject> systemAbility);
+
+    /**
+     * CancelIdle, Cancel Uninstall, Calling the function from idle to active
+     *
+     * @return True indicates Cancel succeeded
+     */
     bool CancelIdle();
+
+    /**
+     * StopAbility, Remove sa from samgr
+     *
+     * @param systemAbilityId,.Said to be deleted
+     * @return void
+     */
     void StopAbility(int32_t systemAbilityId);
 
     SystemAbility(bool runOnCreate = false);
     SystemAbility(int32_t systemAbilityId, bool runOnCreate = false);
     virtual ~SystemAbility();
+
+    /**
+     * GetAbilityState, Obtain status information of sa
+     *
+     * @return status information of sa
+     */
     SystemAbilityState GetAbilityState();
 
 private:
